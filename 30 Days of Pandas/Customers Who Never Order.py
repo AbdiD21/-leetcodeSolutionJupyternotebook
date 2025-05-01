9b8992f9d7830f76eb90294e7af590f1cab7b0f2
Abdi Dabala <abdid.yadata@gmail.com>
2025-05-01 13:17:52 -0700

    p

---
+++ 30 Days of Pandas/Customers Who Never Order.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    # Merge customers with orders on customer id, keeping all customers
+    merged = customers.merge(orders, left_on='id', right_on='customerId', how='left')
+    # Filter where there is no matching order (customerId is NaN)
+    result = merged[merged['customerId'].isna()][['name']]
+    # Rename column as required by output format
+    result.columns = ['Customers']
+    return result
