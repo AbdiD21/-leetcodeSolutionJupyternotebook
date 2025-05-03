020e03cabe11d64d8135249b18fca350bdb238d0
Abdi Dabala <abdid.yadata@gmail.com>
2025-05-03 14:44:26 -0700

    Article Views I

--- 30 Days of Pandas/Article View I.py
+++ 30 Days of Pandas/Article View I.py
@@ -5,3 +5,7 @@ def article_views(views: pd.DataFrame) -> pd.DataFrame:
     result = filtered[['author_id']].drop_duplicates().rename(columns={'author_id': 'id'})
     result = result.sort_values('id').reset_index(drop=True)
     return result
+
+# Time Complexity: O(n log n)
+# Space Complexity: O(n)
+# Where n is the number of rows in the input DataFrame.
